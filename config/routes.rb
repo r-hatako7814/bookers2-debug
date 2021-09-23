f30e2047145deb66b3af9515add1a09af803e79f
r-hatako7814 <ryuichiro.hata@gmail.com>
2021-09-23 16:26:39 +0000

    9/23

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+   root to: 'homes#top'
+   devise_for :users
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+
+
+  get 'homes/about' => 'homes#about'
+
+end

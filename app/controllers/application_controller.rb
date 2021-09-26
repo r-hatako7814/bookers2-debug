978f031d226231c689e2e71f711282f7e64432b1
r-hatako7814 <ryuichiro.hata@gmail.com>
2021-09-26 14:14:21 +0000

    9/26

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    user_path
+    user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
